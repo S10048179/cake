5a603908c7b2498f324c4b09aa4a3b019b3a467c
S10048179 <56732026+S10048179@users.noreply.github.com>
2019-12-11 09:36:43 -0600

    Delete one_line.bat

--- one_line.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "%USERPROFILE%/Documents"
-mkdir "cake"
-powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://raw.githubusercontent.com/S10048179/cake/master/cake.bat' 'cake.bat'; Start-BitsTransfer 'https://raw.githubusercontent.com/S10048179/cake/master/portal-radio.mp3' 'portal-radio.mp3'"
