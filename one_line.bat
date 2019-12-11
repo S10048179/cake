3cf6a828515bb85aa249686b6bb4d0c890d2eb02
S10048179 <56732026+S10048179@users.noreply.github.com>
2019-12-11 09:31:26 -0600

    Add files via upload

--- one_line.bat
+++ one_line.bat
@@ -1,6 +1,4 @@
 @echo off
 cd "%USERPROFILE%/Documents"
 mkdir "cake"
-curl https://raw.githubusercontent.com/S10048179/cake/master/cake.bat --output cake.bat
-curl https://raw.githubusercontent.com/S10048179/cake/master/portal-radio.wav --output portal-radio.mp3
-cake.bat
+powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://raw.githubusercontent.com/S10048179/cake/master/cake.bat' 'cake.bat'; Start-BitsTransfer 'https://raw.githubusercontent.com/S10048179/cake/master/portal-radio.mp3' 'portal-radio.mp3'"
