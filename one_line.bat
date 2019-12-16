c8f281124ba2a03cc3419c30efde8ca3f2eab2fa
S10048179 <56732026+S10048179@users.noreply.github.com>
2019-12-16 08:41:42 -0600

    Update one_line.bat

--- one_line.bat
+++ one_line.bat
@@ -2,3 +2,5 @@
 cd "%USERPROFILE%/Documents/cake"
 powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://raw.githubusercontent.com/S10048179/cake/master/cake.bat' 'cake.bat'; Start-BitsTransfer 'https://raw.githubusercontent.com/S10048179/cake/master/portal-radio.mp3' 'portal-radio.mp3'"
 cake.bat
+sleep 3
+exit
