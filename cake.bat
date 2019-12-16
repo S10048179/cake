2621ad592150c279c72212f187bb911e3599bbc8
S10048179 <56732026+S10048179@users.noreply.github.com>
2019-12-16 08:25:14 -0600

    Update cake.bat

--- cake.bat
+++ cake.bat
@@ -1,6 +1,7 @@
 @echo off
 set "file=portal-radio.mp3"
-( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+( echo MsgBox("You cannot escape the cake.",0,"The Truth")
+  echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
   echo Sound.URL = "%file%"
   echo Sound.settings.volume = 100
   echo Sound.settings.setMode "loop", True
